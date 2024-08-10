c43fbd355a285de1ee13fb1b4ffd2c9e6865570a
Craig Hughes <hghscraig@gmail.com>
2024-08-10 17:23:43 +0100

    fix: work experience content

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -22,7 +22,7 @@ const Layout: React.FC<LayoutProps> = ({children, disableNav}): JSX.Element => (
         <hr />
 
         <footer className="footer footer-center rounded h-[300px] flex items-center justify-center flex-col w-11/12 mx-auto">
-            <p className="badge text-md h-auto bg-follyLight text-folly px-4 py-1 font-bold tracking-tight border-0 flex">
+            <p className="badge text-md h-auto bg-skyLight text-sky px-4 py-1 font-bold tracking-tight border-0 flex">
                 <ThumbsUp className="h-4 w-4" /> Thank you for visiting!
             </p>
             <a
@@ -32,7 +32,7 @@ const Layout: React.FC<LayoutProps> = ({children, disableNav}): JSX.Element => (
                 className="flex text-midnight text-3xl font-light tracking-tight flex-col md:flex-row"
             >
                 Feel free to drop me a message on
-                <span className="underline font-semibold flex items-center gap-2">
+                <span className="underline font-semibold flex items-center gap-2 hover:text-sky transition">
                     LinkedIn <ExternalLink />
                 </span>
             </a>
--- components/homepage/Content.tsx
+++ components/homepage/Content.tsx
@@ -1,11 +1,16 @@
 import {AnimatePresence, motion} from 'framer-motion';
+import Image from 'next/image';
 import {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {GitHub} from 'react-feather';
 
+import THG from '@/assets/thg.png';
+
 import ProjectCard from '../ProjectCard';
 import projects from './Projects';
 import type {Project} from './Projects';
+
+const Highlight = ({children}: {children: string}): JSX.Element => <span className="text-sky">{children}</span>;
 
 const Content: React.FC<PropsWithChildren> = () => {
     const [activeProject, setActiveProject] = useState<Project>();
@@ -20,8 +25,8 @@ const Content: React.FC<PropsWithChildren> = () => {
                 <div className="text-lg">
                     <p>
                         As a Software Engineer, I specialize in full-stack development with{' '}
-                        <span className="text-sky">JavaScript, React, and Next.JS, </span> building features in back-end
-                        systems and CLI tools using <span className="text-sky">Java, Python, and TypeScript</span>.
+                        <Highlight>JavaScript, React, and Next.JS, </Highlight> building features in back-end systems
+                        and CLI tools using <Highlight>Java, Python, and TypeScript</Highlight>.
                     </p>
                     <br />
                     <p>
@@ -33,9 +38,8 @@ const Content: React.FC<PropsWithChildren> = () => {
                     <p>
                         While my day-to-day is filled with opportunities to develop using new and varying technologies,
                         I found myself looking for a new creative challenge in my spare time, leading me to the
-                        development of my game using <span className="text-sky">C# and Unity</span>. I like to think
-                        this mirrors my passion for full-stack development, joining my love of design and programming
-                        skills.
+                        development of my game using <Highlight>C# and Unity</Highlight>. I like to think this mirrors
+                        my passion for full-stack development, joining my love of design and programming skills.
                     </p>
                 </div>
             </article>
@@ -88,46 +92,70 @@ const Content: React.FC<PropsWithChildren> = () => {
                     Work Experience
                 </h2>
 
-                <div>
+                <div className="max-w-[1000px] mx-auto">
                     <ol className="relative border-s border-gray-200 dark:border-gray-700">
                         <li className="mb-10 ms-4">
                             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+                            <div className="h-4 w-[120px] relative my-4">
+                                <Image src={THG.src} alt="THG Ingenuity Logo" fill />
+                            </div>
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                                February 2022
+                                September 2021 - <strong>Current</strong>
                             </time>
-                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                                Application UI code in Tailwind CSS
-                            </h3>
-                            <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-                                Get access to over 20+ pages including a dashboard layout, charts, kanban board,
-                                calendar, and pre-order E-commerce & Marketing pages.
-                            </p>
+                            <h3 className="text-lg font-semibold">Software Engineer @ THG Ingenuity</h3>
+                            <ul className="list-disc ml-10 mt-2 [&>li]:my-8 tracking-tight text-lg">
+                                <li>
+                                    Delivered a new Site Creation tool UI using{' '}
+                                    <Highlight>Next.JS, React, and TypeScript,</Highlight>
+                                    integrating with our internal UI Libraries and adhering to the company design
+                                    system. Introduced custom wrappers to handle library errors and documented the
+                                    process. Ensured a minimum of 80% test coverage using{' '}
+                                    <Highlight>Cypress.JS</Highlight>.
+                                </li>
+                                <li>
+                                    Part of the team that delivered platform migrations for <strong>Cult Beauty</strong>{' '}
+                                    and
+                                    <strong> Lookfantastic</strong>, rebuilding features from the old platform using{' '}
+                                    <Highlight>React, TypeScript, and Next.JS.</Highlight>
+                                </li>
+                                <li>
+                                    Enhanced the platform&apos;s product sizing functionality by integrating third-party
+                                    scripts and modifying our existing <Highlight>Java</Highlight> transformers for
+                                    products to expose the necessary data.
+                                </li>
+                                <li>
+                                    Assisted in transitioning responsibilities to my previous team by supporting
+                                    features and fixes on internal tools. These tools provided platform redirects and a
+                                    front-end interface for our Site Configuration API, automating site setup and
+                                    configuration for new clients using <Highlight>Angular</Highlight> and{' '}
+                                    <Highlight>Java</Highlight>.
+                                </li>
+                                <li>
+                                    Maintained and improved our internal CLI tool to assist the implementation team with
+                                    everyday tasks. Features include switching host file entries while testing sites in
+                                    different environments, identifying unused site configurations, and adding new sites
+                                    to shared <Highlight>JSON</Highlight> files distributed across the database, using
+                                    <Highlight> Python, PyODBC, and Bash</Highlight>.
+                                </li>
+                            </ul>
                         </li>
                         <li className="mb-10 ms-4">
                             <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
+                            <div className="h-4 w-[120px] relative my-4">
+                                <Image src={THG.src} alt="THG Ingenuity Logo" fill />
+                            </div>
                             <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                                March 2022
+                                July 2020 - September 2021
                             </time>
-                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                                Marketing UI design in Figma
-                            </h3>
-                            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                                All of the pages and components are first designed in Figma and we keep a parity between
-                                the two versions even as we update the project.
-                            </p>
-                        </li>
-                        <li className="ms-4">
-                            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 border border-white dark:border-gray-900 dark:bg-gray-700" />
-                            <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                                April 2022
-                            </time>
-                            <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                                E-Commerce UI code in Tailwind CSS
-                            </h3>
-                            <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-                                Get started with dozens of web components and interactive elements built on top of
-                                Tailwind CSS.
-                            </p>
+                            <h3 className="text-lg font-semibold">Graduate Front End Engineer @ THG Ingenuity</h3>
+                            <ul className="list-disc ml-10 mt-2 [&>li]:my-8 tracking-tight text-lg">
+                                <li>
+                                    Collaborated with PMs and site content managers to deliver sites such as{' '}
+                                    <strong>Creed, Endura, Speedo,</strong> and <strong>ZYN</strong>. Site delivery
+                                    consisted of estimation, feature development, and <Highlight>UX and A11Y</Highlight>{' '}
+                                    support post-release.
+                                </li>
+                            </ul>
                         </li>
                     </ol>
                 </div>
